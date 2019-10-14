09de02411e35696d24d9c92fbb4b4de0006b753e
Daniel Nyheim <daniel.nyheim@hotmail.com>
2019-10-14 15:23:28 +0200

    ??

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,15 @@ import './App.css'
 import { Table, Newrow, Toprow, Elt } from './Flexbox';
 import { string, number, array } from 'prop-types';
 import { render } from 'react-dom';
-import { identifier, isTemplateElement } from '@babel/types';
-import { networkInterfaces } from 'os';
 
 const Input = styled.input`
   background-color: transparent;
   color: Azure;
-  
 `
 
  export interface IState {
-  Value: String,
-  search: String,
-  return: String,
-  array: MyArrayValues[]
+   search: String,
+   array: MyArrayValues[]
 }
 
 interface MyArrayValues {
@@ -30,17 +25,9 @@ interface MyArrayValues {
   value3: number,
 }
 
-function updateArray(this: any) {
-
-}
-
-let searchvalue = '1';
-
 export class App extends React.PureComponent{
     state : IState = {
       search: ' ',
-      Value: ' ',
-      return: ' ',
       array: [
         {id: 1, value1: 'Batman', value2: 2000, value3: 200000000},
         {id: 2, value1: 'Titanic', value2: 1999, value3: 10000000},
@@ -52,54 +39,52 @@ export class App extends React.PureComponent{
   onsearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.onsearch  = this.onsearch.bind(this)
     this.setState({
-      search: e.target.value
+      search: e.target.value.toLowerCase()
     })
   }
 
-  ontype = (searchvalue: String, e: React.ChangeEvent<HTMLInputElement>) => {
-    searchvalue.replace('1', e.target.value)
-    console.log(searchvalue)
+  sortid (id: any) {
+    this.sortid = this.sortid.bind(this)
+    const array = this.state.array
+    array.sort((a, b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0)
   }
 
+
   render () {
-    let values = ' '
-    console.log(this.state.return)
-    console.log(this.state.array)
     return (
       <div className='App-header'>
-        <div >
-          <Input placeholder='Search...' 
-            type='text' 
-            onChange={this.onsearch}>
+        <div>
+          <Input
+          type='text'
+          onChange={this.onsearch}
+          >
           </Input>
-          <button></button>
-          {searchvalue}
-          {this.state.search}
-          {this.state.return}
-          {this.state.Value}
         </div>
         <Table>
           <Newrow>
-            <Toprow>nr</Toprow>
-            <Toprow>movie</Toprow>
-            <Toprow>released</Toprow>
-            <Toprow>budget</Toprow>
+            <div onClick={() => this.sortid('id')} >
+              <Toprow>nr</Toprow>
+            </div>
+            <Toprow >movie</Toprow>
+            <Toprow >released</Toprow>
+            <Toprow >budget</Toprow>
           </Newrow>
-            {
-            this.state.array.filter(item =>
-              item.value1.includes('')).map(searchedPlayers => (
-                  <Newrow>
-                    <Elt>{searchedPlayers.id}</Elt>
-                    <Elt>{searchedPlayers.value1}</Elt>
-                    <Elt>{searchedPlayers.value2}</Elt>
-                    <Elt>{searchedPlayers.value3} </Elt>
-                  </Newrow>
-              ))
-            }
-          </Table>
+
+          {
+            this.state.array.map((data) =>
+            <Newrow>
+              <Elt> {data.id} </Elt>
+              <Elt> {data.value1} </Elt>
+              <Elt> {data.value2} </Elt>
+              <Elt> {data.value3} </Elt>
+            </Newrow>
+            )
+          }
+        </Table>
       </div>
     )
   }
 }
 
 export default App;
+
--- src/Flexbox.tsx
+++ src/Flexbox.tsx
@@ -3,11 +3,6 @@ import logo from './logo.svg';
 import { jsx, css } from '@emotion/core'
 import styled from '@emotion/styled'
 
-/*
-    table > div > tr > th > td > your text
-*/
-
-
 export const Table = styled.table`
     border: 2px solid black;
     width: 550px;
@@ -30,37 +25,3 @@ export const Elt = styled.td`
     width: 550px;
 
 `
-
-/* 
--background-size: flex;
-    flex-direction: column;
-    display: flex;
-    text-align: center;
-    overflow: none;
-    padding: 15px;
-
--border-collapse: collapse;
-    border: 1px solid black;
-    width: 600px;
-    border-radius: 0px;
-    display: flex;
-    height: flex;
-    align-items: center;
-
--display: flex;
-    flex-direction: row;
-    flex: 1 1 auto;
-    align-items: center;
-    justify-content: space-between;
-
--border-width: 600;
-    display: flex;
-    flex-direction: row;
-    flex: 1 1 auto;
-    align-items: center;
-    justify-content: space-between;
-
--display: center;
-    flex: 1 1 auto;
-    align-items: center;
-*/
